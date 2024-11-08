0ceadabcf692fd19ed1469202772d812a34e0410
IsratWD <uisratjahan06@gmail.com>
2024-11-08 21:13:51 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// Smooth Scrolling for Navbar Links
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop - 70,
+            behavior: 'smooth'
+        });
+    });
+});
